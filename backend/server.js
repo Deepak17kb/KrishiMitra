25627e7aa91cab0f6cb0ac1662404091ca6ee51b
Deepak <188625906+Deepak17kb@users.noreply.github.com>
2026-03-29 16:48:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ app.use(cors({
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8080",
+    "https://krishi-mitra-silk-nine.vercel.app",  
   ],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
